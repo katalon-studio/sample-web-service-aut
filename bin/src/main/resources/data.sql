3df9a4fc51455d40c027405bbc6e0d77a8b9f56c
hieudbui99 <hieudbui@kms-technology.com>
2020-02-20 16:28:51 +0700

    Update namespace, gender and version in xml

--- bin/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into user(username, password, gender, age, avatar) values ('John Smith', '123', 0, 25, null);
-insert into user(username, password, gender, age, avatar) values ('Maria Rodriguez', '456', 0, 55, null);
-insert into user(username, password, gender, age, avatar) values ('James Johnson', '789', 1, 75, null);
