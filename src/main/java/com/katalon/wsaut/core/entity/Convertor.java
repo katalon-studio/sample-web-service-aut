30bd8fdc6af0a6d48c5567ae42f53bd9b6830e0e
HieuBui419 <hieu.bui@katalon.com>
2021-08-03 15:04:49 +0700

    fix Converter

--- src/main/java/com/katalon/wsaut/core/entity/Convertor.java
+++ src/main/java/com/katalon/wsaut/core/entity/Convertor.java
@@ -6,6 +6,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
+import javax.annotation.PostConstruct;
 import javax.imageio.ImageIO;
 
 import org.modelmapper.AbstractConverter;
@@ -20,17 +21,13 @@ public class Convertor {
 	private ModelMapper modelMapper = new ModelMapper();
 	
 	public UserSOAP convertToSOAP(User entity) {
-		//UserSOAP user = new UserSOAP();
-		modelMapper.addConverter(toImage);
-		modelMapper.createTypeMap(Byte.class, Image.class);
+		//UserSOAP user = new UserSOAP();		
 		UserSOAP user = modelMapper.map(entity, UserSOAP.class);		
 		return user;
 	}
 	
 	public User convertToEntity(UserSOAP user) {
-		//User entity = new User();
-		modelMapper.addConverter(toBytes);
-		modelMapper.createTypeMap(Image.class, Byte.class);
+		//User entity = new User();		
 		User entity = modelMapper.map(user, User.class);		
 		return entity;
 	}
@@ -47,31 +44,45 @@ public class Convertor {
 
 		@Override
 		protected Image convert(byte[] source) {
-			ByteArrayInputStream bis = new ByteArrayInputStream(source);
-			BufferedImage image = null;
-			try {
-				image = ImageIO.read(bis);
-			} catch (IOException e) {
-				e.printStackTrace();
+			if (source != null) {
+				ByteArrayInputStream bis = new ByteArrayInputStream(source);
+				BufferedImage image = null;
+				try {
+					image = ImageIO.read(bis);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				return image;
 			}
-			return image;
+			return null;
 		}
 	};
 	
 	Converter<Image, byte[]> toBytes = new AbstractConverter<Image, byte[]>() {
 		@Override
 		protected byte[] convert(Image source) {
-			byte[] bytes = null;
-			if (source instanceof BufferedImage) {
-				ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		        try {
-					ImageIO.write((BufferedImage) source, "jpg", baos);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-		        bytes = baos.toByteArray();
-			}			
-	        return bytes;
+			if (source != null) {
+				byte[] bytes = null;
+				if (source instanceof BufferedImage) {
+					ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			        try {
+						ImageIO.write((BufferedImage) source, "jpg", baos);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+			        bytes = baos.toByteArray();
+				}			
+		        return bytes;
+			}
+			return null;
 		}
 	};
+	
+	@PostConstruct
+	private void initConverter() {
+		modelMapper.addConverter(toBytes);
+		modelMapper.createTypeMap(Image.class, Byte.class);
+		modelMapper.addConverter(toImage);
+		modelMapper.createTypeMap(Byte.class, Image.class);
+	}
 }
